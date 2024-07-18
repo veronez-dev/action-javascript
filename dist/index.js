7b2de7f6494b632cd71a7d3471556a4687cdf7f0
Fabricio Veronez <fabricioveronez@gmail.com>
2024-07-18 16:48:39 -0300

    v2.0.0

--- dist/index.js
+++ dist/index.js
@@ -31087,35 +31087,35 @@ async function run() {
     const token = core.getInput('GITHUB_TOKEN');
     const issueId = core.getInput('issue-id');
     const comment = core.getInput('comment');
-    
+
     // Inicializa o cliente do GitHub
     const octokit = github.getOctokit(token);
-    
+
     // Obtem o contexto do repositório
     const context = github.context;
     const { owner, repo } = context.repo;
-    
+
     // Adiciona um comentário ao issue ou pull request
     const response = await octokit.rest.issues.createComment({
-        owner: owner,
-        repo: repo,
-        issue_number: issueId,
-        body: comment,
-      });
-      
-      // Obtenha o ID do comentário criado
-      const commentId = response.data.id;
-      
-      // Define o output com o ID do comentário
-      core.setOutput('comment-id', commentId);
-      
-      console.log('Comentário adicionado com sucesso! ID do comentário:', commentId);
-    } catch (error) {
-      core.setFailed(`Erro ao adicionar comentário: ${error.message}`);
-    }
+      owner: owner,
+      repo: repo,
+      issue_number: issueId,
+      body: comment + " Versão 2.0",
+    });
+
+    // Obtenha o ID do comentário criado
+    const commentId = response.data.id;
+
+    // Define o output com o ID do comentário
+    core.setOutput('comment-id', commentId);
+
+    console.log('Comentário adicionado com sucesso! ID do comentário:', commentId);
+  } catch (error) {
+    core.setFailed(`Erro ao adicionar comentário: ${error.message}`);
   }
-  
-  run();
+}
+
+run();
 
 })();
 
